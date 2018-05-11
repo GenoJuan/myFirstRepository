768923bcafa9ffa0b134ffb37bb7f29deb61e5d1
Juan Arias <jariasdelangel@gmail.com>
2018-05-11 16:06:05 -0500

    I am evil
    I am evil and so I deleted a whole file.

--- myFirstProgramInR.r
+++
@@ -1,7 +0,0 @@
-# Dear future Juan,
-# If you are reading this. Congratulations! You're using GitHub and hopefully you are ordering your work.
-# This is an almost empty script.
-
-library(BoolNet)
-
-net = loadNetwork("file.net")
